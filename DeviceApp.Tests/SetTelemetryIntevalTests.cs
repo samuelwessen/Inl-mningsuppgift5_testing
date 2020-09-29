9e3ee0afcade668db11789c1c52bda925a541ad1
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-29 11:22:25 +0200

    inlamingsuppgift5

--- DeviceApp.Tests/SetTelemetryIntevalTests.cs
+++ DeviceApp.Tests/SetTelemetryIntevalTests.cs
@@ -1,6 +1,8 @@
 using DeviceApp.Services;
 using Microsoft.Azure.Devices.Client;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace DeviceApp.Tests
@@ -9,12 +11,19 @@ namespace DeviceApp.Tests
     {
         private DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=ec-win20-samuelw-iothub.azure-devices.net;DeviceId=Inlamningsuppgift5;SharedAccessKey=jlfiXyYmYZ91dLSZvLgaDHhTR/dNniRQB0b9rPuMDUI=", TransportType.Mqtt);
 
+        
+
         [Theory]
         [InlineData("SetTelemetryInterval","10", 200)]
         [InlineData("SetInterval", "10", 501)]
         public void SetTelemetryInterval_ShouldChangeTheInterval(string methodName, string payload, int statusCode)
         {
-            deviceClient.SetMethodHandlerAsync(methodName, DeviceService.SetTelemetryInterval, null).Wait();
+            var response = DeviceService.SetTelemetryInterval(new MethodRequest(methodName), null).Result;
+            Assert.Equal(statusCode, response.Status);
+
         }
     }
+
+   // vi ska få in payload i MethodRequest delen, kolla inspelningen där vi använder MethodRequest
+    
 }
--- DeviceApp/Services/DeviceService.cs
+++ DeviceApp/Services/DeviceService.cs
@@ -9,9 +9,28 @@ namespace DeviceApp.Services
 {
     public class DeviceService
     {
-        public static Task<MethodResponse> SetTelemetryInterval(MethodRequest methodRequest, object userContext)
+        private static DeviceClient deviceclient = DeviceClient.CreateFromConnectionString("HostName=ec-win20-samuelw-iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=1PoRNau16h+eqaQOX4BbIbPenmV6UukCFOM6X2wiQKc=", TransportType.Mqtt);
+        private static int telemetryInterval = 5;
+        private static Random rnd = new Random();
+
+
+        public static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext)
         {
-            throw new NotImplementedException();
+            var payload = Encoding.UTF8.GetString(request.Data).Replace("\"", "");
+
+            if (Int32.TryParse(payload, out telemetryInterval))
+            {
+                Console.WriteLine($"Interval set to: {telemetryInterval} seconds");
+
+                // { "result": "Executed direct method: SetTelemetryInterval" }
+                string json = "{\"result\": \"Executed direct method: " + request.Name + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 200));
+            }
+            else
+            {
+                string json = "{\"result\": \"Method not implemented\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 501));
+            }
         }
     }
 }
